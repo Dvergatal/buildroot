1ada4aadbd0dfb3ebb8c7f111899e4e729f55b0a
Piotr Łobacz <piotr.lobacz@softgent.com>
2020-11-02 09:54:24 +0100

    added optee_client to the packages

---
+++ package/optee_client/optee_client.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# optee_client
+#
+################################################################################
+
+OPTEE_CLIENT_VERSION = f2a27ab40c1613c60f8ae25cab4b695f773457ed
+OPTEE_CLIENT_SITE = git://github.com/etienne-lms/optee_client.git
+
+OPTEE_CLIENT_LICENSE = GPLv2
+OPTEE_CLIENT_LICENSE_FILES = GPL-COPYING
+
+OPTEE_CLIENT_CONF_OPTS += -DCMAKE_C_COMPILER=aarch64-linux-gnu-
+
+$(eval $(cmake-package))
