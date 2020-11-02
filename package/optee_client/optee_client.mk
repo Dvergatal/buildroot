12c4abc095620fd015816179e7463075345425a2
Piotr Łobacz <piotr.lobacz@softgent.com>
2020-11-02 14:37:49 +0100

    added support for openssl-1.1.1h, modified optee_client package

--- package/optee_client/optee_client.mk
+++ package/optee_client/optee_client.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 OPTEE_CLIENT_VERSION = f2a27ab40c1613c60f8ae25cab4b695f773457ed
-OPTEE_CLIENT_SITE = git://github.com/etienne-lms/optee_client.git
+OPTEE_CLIENT_SITE = $(call github,Dvergatal,optee_client,$(OPTEE_CLIENT_VERSION))
 
 OPTEE_CLIENT_LICENSE = GPLv2
 OPTEE_CLIENT_LICENSE_FILES = GPL-COPYING
