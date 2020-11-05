d5d918388864ab222b8df0c7831a1ddf78053caa
Piotr Łobacz <piotr.lobacz@softgent.com>
2020-11-05 17:33:03 +0100

    added modified optee_client package

--- package/optee_client/optee_client.mk
+++ package/optee_client/optee_client.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPTEE_CLIENT_VERSION = 3fdc84987b6581a609ba583bbdc74bb28cc388b1
+OPTEE_CLIENT_VERSION = ba21c8eb77bbc175b431cf6007b7a3ca5d6e4b11
 OPTEE_CLIENT_SITE = $(call github,Dvergatal,optee_client,$(OPTEE_CLIENT_VERSION))
 
 OPTEE_CLIENT_LICENSE = GPLv2
